a5c2729fac228837d13503da2daa5d0a5abda8db
diegoacosta127 <diegoacosta127@gmail.com>
2023-02-24 14:13:39 -0300

    Revert "bluebird lib added to handle promises"
    This reverts commit aaa3869c89296330651f5ae88a0cf08ac97a087b.
    
    revert bluebird lib pushed

--- bot.js
+++ bot.js
@@ -1,11 +1,6 @@
 const axios = require('axios').default;
 const cheerio = require('cheerio');
 const { Telegraf } = require('telegraf');
-const Promise = require('bluebird');
-  Promise.config({
-    cancellation: true
-  });
-
 
 const BASE_URL = 'https://api.musixmatch.com/ws/1.1';
 
