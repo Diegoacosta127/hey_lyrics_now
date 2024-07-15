f4221ff49c4fa7fbab435b2b88f8669f57f381bf
diegoacosta127 <diegoacosta127@gmail.com>
2024-07-15 00:35:37 -0300

    just another test

--- bot.js
+++ bot.js
@@ -32,12 +32,6 @@ const pool = new Pool({
   port: 13211
 })
 
-console.log("host :", POSTGRES_HOST);
-console.log("db :", POSTGRES_DB);
-console.log("user :", POSTGRES_USER);
-console.log("pass :", POSTGRES_PASSWORD);
-console.log("port :", PGPORT);
-
 // Conexión con Telegram
 const token = '6095592859:AAG7ImwPScy04HbRgD0ZJaPSTpPIjkuLvYA';
 const bot = new Telegraf(token, {polling: true});
